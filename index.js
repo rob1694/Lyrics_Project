0daa8d048b225b761ff23cd6a3274df6b913a0e5
rob1694 <robertb1694@gmail.com>
2021-04-19 20:34:50 -0400

    added basic html For visual

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const lyric_API = 'https://api.lyrics.ovh/v1/';
 let artist = '', song = '';
 
-function getLyrics(artist, song){
-fetch(`${lyric_API}${artist}/${song}`)
-    .then(resp => resp.json())
-    .then(data => console.log(data))
-}
+// function getLyrics(artist, song){
+// fetch(`${lyric_API}${artist}/${song}`)
+//     .then(resp => resp.json())
+//     .then(data => console.log(data))
+// }
 
-getLyrics('coldplay', 'Adventure of a Lifetime')
+// getLyrics('coldplay', 'Adventure of a Lifetime')
