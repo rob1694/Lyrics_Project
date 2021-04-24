9254d464ca24986959fc34e4f9cddce8c014f76c
rob1694 <robertb1694@gmail.com>
2021-04-24 17:47:42 -0400

    added comments section

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     getLyrics(artist, song)
         .then(displayLyrics)
         replaceLyrics()
+        displayComment()
     })
 
 function getLyrics(artist, song){
@@ -72,3 +73,47 @@ function replaceLyrics(){
             clearSearchInfo()
 })
 }
+
+/*
+create p tag with comment input
+append p tag on click
+add event listener that displays comment
+reset comment form after click
+*/
+
+function createComment(comments) {
+    const section = document.getElementById('comments-section')
+    const comment = document.createElement('p'),
+    div = document.createElement('div')
+
+    comment.textContent = comments
+
+    section.appendChild(div)
+    div.appendChild(comment)
+
+    return div
+}
+
+function getComment() {
+    let commentInput = document.querySelector('.comment-control');
+
+        comment = commentInput.value
+
+        return comment;
+    }
+
+function clearCommentInput() {
+    document.querySelector('#comments-form').reset();
+}
+
+function displayComment() {
+    const submit = document.getElementById('submit-btn');
+        
+    submit.addEventListener("click", (e) => {
+        e.preventDefault();
+        const comment = getComment();
+        createComment(comment);
+        clearCommentInput();
+    })
+}
+
